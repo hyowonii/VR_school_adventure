45901b93fa5bf6b0d7734571b159c5060cf2cf58
Plammet <ohjs2000@gmail.com>
2022-04-12 13:28:11 +0900

    new assests

---
+++ Assets/Puxxe_Studio/3D_Low_Characters/Scripts/CharactersSwipeMenu.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CharactersSwipeMenu : MonoBehaviour{
+	
+    [SerializeField]
+    bool autoRotation = true;
+    public bool smoothSpeedAnimation = false;
+    [SerializeField]
+    Toggle rotateToggle;
+    [SerializeField]
+    Toggle speedToggle;
+    [SerializeField]
+    bool hideOthersCharacters = true;
+    [SerializeField]
+    Text characterInfoText;
+    [SerializeField]
+    GameObject[] charactersList = null;
+    int characterID = 0;
+    float rotationAtual = -90;
+    public PlayerControllDemo playerControllDemo;
+    GameObject character;
+    int animatorActionID = 0;
+    float animatorSpeed;
+    Transform characterTransform;
+	
+    private void Awake(){
+        character = charactersList[characterID];
+        playerControllDemo = charactersList[characterID].GetComponent<PlayerControllDemo>();
+        getPreviousCharacterValues();
+    }
+    void Start(){
+        getPreviousCharacterValues();
+        HideCharacters();
+        ShowAtualCharacter();
+    }
+    void HideCharacters(){
+        if (charactersList != null){
+            for (int i = 0; i < charactersList.Length; i++){
+                GameObject character = charactersList[i];
+                character.transform.parent.rotation = new Quaternion(0, rotationAtual, 0, 0);
+                if (hideOthersCharacters == true){
+                    character.SetActive(false);
+                }
+            }
+        }
+        ShowCharacterInfo();
+    }
+    void ShowAtualCharacter(){
+        if (charactersList != null){
+            character = charactersList[characterID];
+            playerControllDemo = character.GetComponent<PlayerControllDemo>();
+            playerControllDemo.FindComponents();
+            if (playerControllDemo == null){
+                Debug.LogWarning("playerControllDemo NOT FOUND!");
+            }
+            character.SetActive(true);
+            character.transform.position = characterTransform.position;
+            character.transform.rotation = characterTransform.rotation;
+            playerControllDemo.animator.speed = animatorSpeed;
+            SetActionInt(animatorActionID);
+        }
+        ShowCharacterInfo();
+    }
+    void getPreviousCharacterValues(){
+        characterTransform = character.transform;
+        animatorSpeed = playerControllDemo.animator.speed;
+        animatorActionID = playerControllDemo.actionID;
+    }
+    void ShowCharacterInfo(){
+        if (characterInfoText != null){
+            characterInfoText.text = "Character " + (characterID + 1) + "/" + charactersList.Length;
+        }
+    }
+    public void PreviousCharacter(){
+        getPreviousCharacterValues();
+        if (characterID > 0){
+            characterID--;
+            HideCharacters();
+            ShowAtualCharacter();
+        }
+    }
+    public void NextCharacter(){
+        getPreviousCharacterValues();
+        if (characterID < charactersList.Length - 1){
+            characterID++;
+            HideCharacters();
+            ShowAtualCharacter();
+        }
+    }
+    public void Jump(){
+        playerControllDemo.Jump();
+    }
+    public void SetActionInt(int _actionID = -1){
+        playerControllDemo.SetActionInt(_actionID);
+    }
+    public void SetActionName(string _actionName = "idle_01"){
+        playerControllDemo.SetActionName(_actionName);
+    }
+    public void ToogleRotateCharacter(bool value){
+        autoRotation = value;
+    }
+    public void ToogleSmoothSpeedAnimation(bool value){
+        smoothSpeedAnimation = value;
+        if (smoothSpeedAnimation == true){
+            SetAnimatorSpeed(0.2f);
+        }else{
+            SetAnimatorSpeed(1.0f);
+        }
+    }
+    public void RotateCharacter(float rotation = -1){
+        GameObject character = charactersList[characterID];
+        if (character.activeSelf == true){ 
+            if (rotation != -1){
+                rotationAtual = character.transform.rotation.y;
+                rotationAtual += rotation;
+                character.transform.RotateAround(transform.position, transform.up, rotationAtual);
+            }else{
+                character.transform.RotateAround(transform.position, transform.up, Time.deltaTime * -90f);
+                rotationAtual = character.transform.rotation.y;
+            }
+        }
+    }
+    public void SetAnimatorSpeed(float _speed = 1){
+        playerControllDemo.SetAnimatorSpeed(_speed);
+    }
+    void Update(){
+        GetKeyBoard();
+        if (autoRotation){
+            RotateCharacter();
+        }
+    }
+    void GetKeyBoard(){
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            PreviousCharacter();
+        }else if (Input.GetKeyDown(KeyCode.RightArrow)){
+            NextCharacter();
+        }
+        if (Input.GetKeyDown(KeyCode.R)){
+            ToogleRotateCharacter(!autoRotation);
+            if (rotateToggle != null){
+                rotateToggle.isOn = autoRotation;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S)){
+            ToogleSmoothSpeedAnimation(!smoothSpeedAnimation);
+            if (speedToggle != null){
+                speedToggle.isOn = smoothSpeedAnimation;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.T)){
+            SetActionName("_none");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1)){
+            SetActionName("idle_01");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)){
+            SetActionName("walk_01");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3)){
+            SetActionName("run_01");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4)){
+            Jump();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5)){
+			 // SetActionName("spin_01"); //NOTE USED
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6)){
+            SetActionName("attack_01");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7)){
+            SetActionName("hit_01");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8)){
+            SetActionName("lose_01");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9)){
+            SetActionName("die_01");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0)){
+            SetActionName("win_dance_01");
+        }
+    }
+}
+
+
---
+++ Assets/Puxxe_Studio/3D_Low_Characters/Scripts/PlayerControllDemo.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerControllDemo : MonoBehaviour{
+    [SerializeField]
+    public Animator animator;
+    [SerializeField]
+    Rigidbody rigidbody;
+    [SerializeField]
+    private float jumpForce = 7f;
+    bool isGrounded = false;
+    Dictionary<string, int> actions = new Dictionary<string, int>();
+    [HideInInspector]
+    public int actionID = 0;
+    [Header("Actions Names")]
+    string NONE = "_none";
+    string IDLE_1 = "idle_01";
+    string WALK_1 = "walk_01";
+    string RUN_1 = "run_01";
+    string JUMP_1 = "jump_01";
+    string JUMP_1_FALL = "jump_01_fall";
+    string JUMP_1_LAND = "jump_01_land";
+    string SPIN_1 = "spin_01";//NOT USED
+    string ATTACK_1 = "attack_01";
+    string HIT_1 = "hit_01";
+    string LOSE_1 = "lose_01";
+    string DIE_1 = "die_01";
+    string WIN_DANCE_1 = "win_dance_01";
+    [Header("Actions ID")]
+    int NONE_ID = 0;
+    int IDLE_1_ID = 1;
+    int WALK_1_ID = 2;
+    int RUN_1_ID = 3;
+    int JUMP_1_ID = 4;
+    int JUMP_1_FALL_ID = 5;
+    int JUMP_1_LAND_ID = 6;
+    int SPIN_1_ID = 7;//NOT USED
+    int ATTACK_1_ID = 8;
+    int HIT_1_ID = 9;
+    int LOSE_1_ID = 10;
+    int DIE_1_ID = 11;
+    int WIN_DANCE_1_ID = 12;
+    string backActionName = "idle_01";
+    int backActionID = 1;
+    private void Awake(){
+        gameObject.transform.position = new Vector3(0, 5, 0);
+        FindComponents();
+        actions[NONE] = NONE_ID;
+        actions[IDLE_1] = IDLE_1_ID;
+        actions[WALK_1] = WALK_1_ID;
+        actions[RUN_1] = RUN_1_ID;
+        actions[JUMP_1] = JUMP_1_ID;
+        actions[JUMP_1_FALL] = JUMP_1_FALL_ID;
+        actions[JUMP_1_LAND] = JUMP_1_LAND_ID;
+        actions[SPIN_1] = SPIN_1_ID;//NOT USED
+        actions[ATTACK_1] = ATTACK_1_ID;
+        actions[HIT_1] = HIT_1_ID;
+        actions[LOSE_1] = LOSE_1_ID;
+        actions[DIE_1] = DIE_1_ID;
+        actions[WIN_DANCE_1] = WIN_DANCE_1_ID;
+        backActionName = IDLE_1;
+        backActionID = IDLE_1_ID;
+        UpdateAnimationAction();
+    }
+    void Start(){
+        UpdateAnimationAction();
+    }
+    public void FindComponents(){ 
+        rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        if (animator == null){
+            animator = GetComponentInChildren<Animator>();
+        }
+    }
+    public void Jump(){
+        if (isGrounded == true){
+            rigidbody.velocity = new Vector3(0f, jumpForce, 0f);
+            isGrounded = false;
+            SetActionName(JUMP_1);
+        }else if (actionID == (int)actions[JUMP_1]){
+			// SetActionName(SPIN_1);//NOTE USED
+        }
+    }
+    void Update(){
+        UpdateAnimationAction();
+    }
+    public void SetActionInt(int _actionID = -1){
+        StopCoroutine("ReturnToActionCoroutine");
+        actionID = _actionID;
+        animator.SetInteger("actionID", actionID);
+        if (actionID == (int)actions[ATTACK_1] || actionID == (int)actions[HIT_1] || actionID == (int)actions[LOSE_1]){  
+            if (FindObjectOfType<CharactersSwipeMenu>().smoothSpeedAnimation == true){
+                ReturnToAction(IDLE_1, 3.5f);
+            }else{
+                ReturnToAction(IDLE_1, 1.5f);
+            }
+        }
+        UpdateAnimationAction();
+    }
+    public void SetActionName(string _actionName = "idle_01"){
+        StopCoroutine("ReturnToActionCoroutine");
+        actionID = (int)actions[_actionName];
+        animator.SetInteger("actionID", actionID);
+        if (actionID == (int)actions[ATTACK_1] || actionID == (int)actions[HIT_1] || actionID == (int)actions[LOSE_1]){
+            if (FindObjectOfType<CharactersSwipeMenu>().smoothSpeedAnimation == true){
+                ReturnToAction(IDLE_1, 3.5f);
+            }else{
+                ReturnToAction(IDLE_1, 1.5f);
+            }
+        }
+        UpdateAnimationAction();
+    }
+    public void SetAnimatorSpeed(float _speed = 1){
+        animator.speed = _speed;
+    }
+    private void UpdateAnimationAction(){
+        if (rigidbody.velocity.y > .1f){
+            if (actionID != (int)actions[JUMP_1]){
+                actionID = (int)actions[JUMP_1];
+                SetActionName(JUMP_1);
+            }
+        }else if (rigidbody.velocity.y < -.1f){
+            if (actionID != (int)actions[JUMP_1_FALL]){
+                SetActionName(JUMP_1_FALL);
+            }
+        }
+        if (actionID == (int)actions[SPIN_1]){
+			//NOTE USED
+        }
+        if (transform.position.y <= -10){
+            RestarLevel();
+        }
+    }
+    void ReturnToAction(string _actionName = "idle_01", float _returnTime = 2.0f){
+        backActionName = _actionName;
+        backActionID = (int)actions[_actionName];
+        StopCoroutine("ReturnToActionCoroutine");
+        StartCoroutine("ReturnToActionCoroutine", _returnTime);
+    }
+    IEnumerator ReturnToActionCoroutine(float _returnTime = 3.0f){
+        yield return new WaitForSeconds(_returnTime);
+        if (backActionName != ""){
+            SetActionName(backActionName);
+        }else if (backActionID != -1){
+            SetActionInt(backActionID);
+        }
+    }
+    private void OnCollisionEnter(Collision collision){
+        if (collision.transform.CompareTag("Ground")){
+            isGrounded = true;
+            if (actionID == (int)actions[JUMP_1_FALL]){
+                SetActionName(JUMP_1_LAND);
+                ReturnToAction(IDLE_1, 1.0f);
+            }
+        }
+    }
+    private void OnCollisionExit(Collision collision){
+        if (collision.transform.CompareTag("Ground")){
+            isGrounded = false;
+        }
+    }
+    public void RestarLevel(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
